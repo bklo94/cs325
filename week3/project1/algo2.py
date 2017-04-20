e95c1285b363dc6473cd7ce19f70c5a8fea553ce
Victor Ness <vjness101@gmail.com>
2017-04-19 20:20:35 -0400

    Added I/O functionality to algorithm 2

--- week3/project1/algo2.py
+++ week3/project1/algo2.py
@@ -6,22 +6,22 @@ CS325
 """
 
 
-def max_sum_subarray(input_list):
+def max_sum_subarray(arr):
 
     """
-    This implementation of max sum subarray takes a list as input and finds the sum of the max sum
+    This implementation of max sum subarray takes an array as input and finds the sum of the max sum
     subarray within. It utilizes bottom-up dynamic programming with hash maps to store previously computed
     sums for more efficiency compared to the naive enumeration algorithm.
     """
 
-    if len(input_list) != 0:
-        max_sum = input_list[0]
-        max_list = input_list[0:1]
+    if len(arr) != 0:
+        max_sum = arr[0]
+        max_subarray = arr[0:1]
     current_sum = 0
     sums_dict = {}
 
-    for idx_out, val_out in enumerate(input_list):
-        for idx_in, val_in in enumerate(input_list):
+    for idx_out, val_out in enumerate(arr):
+        for idx_in, val_in in enumerate(arr):
             if idx_in >= idx_out:
                 if idx_out == idx_in:
                     sums_dict[(idx_out, idx_in)] = val_in
@@ -32,12 +32,33 @@ def max_sum_subarray(input_list):
 
                 if current_sum > max_sum:
                     max_sum = current_sum
-                    max_list = input_list[idx_out:(idx_in+1)]
+                    max_subarray = arr[idx_out:(idx_in+1)]
 
                 current_sum = 0
 
-    return max_list, max_sum
+    return (max_subarray, max_sum)
 
-test_input = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
 
-print max_sum_subarray(test_input)
+def process_input():
+
+    """
+    reads input from file MSS_Problems.txt and converts each line to an array. Finds the maximum sum subarray 
+    for the input array and outputs the original array, max subarray, and max sum into file
+    MSS_Results.txt
+    """
+
+    inputfile = open('MSS_Problems.txt')
+    outputfile = open('MSS_Results.txt', 'a')
+
+    for line in inputfile:
+        input_array = map(int, line.strip().split(' '))
+        outputs = max_sum_subarray(input_array)
+
+        outputfile.write(str(input_array) + '\n')
+        outputfile.write(str(outputs[0]) + '\n')
+        outputfile.write(str(outputs[1]) + '\n\n')
+
+    inputfile.close()
+    outputfile.close()
+
+process_input()
